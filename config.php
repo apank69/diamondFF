612c7f35105d252ca99fcc1369b7b3a082b5343c
apank69 <33617545+apank69@users.noreply.github.com>
2019-09-29 22:56:22 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$cointoken = "ee8a424ad91fa49ce1198ea6c7b77ee3c6a4f2e8";
+$PHPSESSID = "PHPSESSID=trk9ohaqlf2qahf2nstbuh9d6i";
+$cfduid = "__cfduid=dbd0a6d39c961556c892a8129dbd14b2e1569748002";
+$useragend = "Mozilla/5.0 (Linux; Android 8.1.0; vivo 1808) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36"
+
+
+?>
